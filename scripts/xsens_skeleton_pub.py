ad8a5a1c608d2696f243c3cdb911adad3ebaccf7
tony <470963660@qq.com>
2018-05-28 17:41:12 +0800

    tmp

--- scripts/xsens_skeleton_pub.py
+++ scripts/xsens_skeleton_pub.py
@@ -30,35 +30,39 @@ if __name__ == '__main__':
     listener = tf.TransformListener()
     broadcaster = tf.TransformBroadcaster()
     r = rospy.Rate(10)
+    xsense_tf_names = ["pelvis_xsens", "l5_abdomen_down_xsens", "l3_abdomen_up_xsens", "t12_sternum_down_xsens", "t8_sternum_up_xsens", "neck_xsens", "head_xsens",
+    "right_shoulder_xsens", "right_upper_arm_xsens", "right_forearm_xsens", "right_hand_xsens", "left_shoulder_xsens", "left_upper_arm_xsens", "left_forearm_xsens",
+    "left_hand_xsens", "right_upper_leg_xsens", "right_lower_leg_xsens", "right_foot_xsens", "right_toe_xsens", "left_upper_leg_xsens", "left_lower_leg_xsens", 
+    "left_foot_xsens", "left_toe_xsens"]
+
+    xsense_tf_mat = [None] * 23
 
     while not rospy.is_shutdown():
             writable.clear()
             msg = copy.deepcopy(shared_msg)
             writable.set()
-
-            if msg is not None:
-                #ipdb.set_trace()
-                look_up_t = rospy.Time(0)
-                listener.waitForTransform('body_sonser', 't8', look_up_t, rospy.Duration(3))
-                t8_to_bodysensor = listener.lookupTransform('body_sonser', 't8', look_up_t)
-                t8_to_bodysensor_mat = listener.fromTranslationRotation(*t8_to_bodysensor) 
-
-
-                for marker in msg.markers:
-                    pose = marker.pose.pose
-                    pos = pose.position
-                    ori = pose.orientation
-                    base_to_marker_mat = numpy.dot(translation_matrix((pos.x, pos.y, pos.z)), quaternion_matrix((ori.x, ori.y, ori.z, ori.w)))
-
-                    t8_to_bodysensor_mat_inv = inv(t8_to_bodysensor_mat)
-                    bodysensor_base = numpy.dot(base_to_marker_mat,t8_to_bodysensor_mat_inv)
             
-                    trans = translation_from_matrix(bodysensor_base)
-                    quat = quaternion_from_matrix(bodysensor_base)
-                    broadcaster.sendTransform(
-                        trans,
-                        quat,
-                        rospy.Time.now(),
-                        'body_sonser',
-                        'base', 
-                    )
+            for tf_index,tf_name in enumerate(xsense_tf_names):
+                look_up_t = rospy.Time(0)
+                listener.waitForTransform('body_sensor', tf_name, look_up_t, rospy.Duration(3))
+                xsense_tf_mat[tf_index] = listener.lookupTransform('body_sensor', tf_name, look_up_t)
+                xsense_tf_mat[tf_index] = listener.fromTranslationRotation(*xsense_tf_mat[tf_index]) 
+
+            for marker in msg.markers:
+                pose = msg.markers[0].pose.pose
+                pos = pose.position
+                ori = pose.orientation
+                base_to_marker_mat = numpy.dot(translation_matrix((pos.x, pos.y, pos.z)), quaternion_matrix((ori.x, ori.y, ori.z, ori.w)))
+
+                t8_to_bodysensor_mat_inv = inv(t8_to_bodysensor_mat)
+                bodysensor_base = numpy.dot(base_to_marker_mat,t8_to_bodysensor_mat_inv)
+        
+                trans = translation_from_matrix(bodysensor_base)
+                quat = quaternion_from_matrix(bodysensor_base)
+                base_xsen = numpy.hstack((trans,quat))
+                print "________"
+                print "base to xsen transform"
+                print base_xsen
+                print "________"
+
+                    
