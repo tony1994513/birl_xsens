27bb7cde125673ee2e6c5ae1a01551b7bfcc0758
tony <470963660@qq.com>
2018-05-30 10:20:55 +0800

    add src which is xsens_tf_broadcaster

---
+++ src/header.h
@@ -0,0 +1,34 @@
+typedef struct header {
+	char ID_String[6];
+	int sample_counter;
+	char datagram_counter;
+	char number_of_items;
+	int time_code;
+	char character_ID;
+	char reserved_for_future_use[7];
+}Header;
+//Header h;
+
+//struct header {
+//	char ID_String[6];
+//	int sample_counter;
+//	char datagram_counter;
+//	char number_of_items;
+//	int time_code;
+//	char character_ID;
+//	char reserved_for_future_use[7];
+//
+//};
+//struct header h;
+
+//struct header {
+//	char ID_String[6];
+//	int sample_counter;
+//	char datagram_counter;
+//	char number_of_items;
+//	int time_code;
+//	char character_ID;
+//	char reserved_for_future_use[7];
+//
+//}Header;
+//Header h;
